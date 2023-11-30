939c38a2760e94281c4c0067470e3463a9f3039f
unknown <jirjisrojulatiq@gmail.com>
2023-12-01 00:28:26 +0700

    ril

---
+++ Modul 4/Prak/prak2/generator.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace MyApp;
+
+// Trait
+trait Loggable {
+    public function log($message) {
+        echo "[Log] $message\n";
+    }
+}
+
+// Abstract Class
+abstract class Animal {
+    // Properties
+    protected $name;
+    protected $sound;
+
+
+    abstract public function makeSound();
+    abstract public function move();
+
+
+    public function __construct($name, $sound) {
+        $this->name = $name;
+        $this->sound = $sound;
+    }
+
+    
+    public function introduce() {
+        return "I am $this->name!";
+    }
+}
+
+
+class Dog extends Animal {
+    use Loggable; 
+
+
+    public function __construct($name) {
+        parent::__construct($name, 'Bark');
+    }
+
+
+    public function makeSound() {
+        $this->log("Dog is barking: $this->sound");
+        return $this->sound;
+    }
+
+    public function move() {
+        $this->log("Dog is walking.");
+    }
+}
+
+class Cat extends Animal {
+    use Loggable; 
+
+
+    public function __construct($name) {
+        parent::__construct($name, 'Meow');
+    }
+
+
+    public function makeSound() {
+        $this->log("Cat is meowing: $this->sound");
+        return $this->sound;
+    }
+
+    public function move() {
+        $this->log("Cat is jumping.");
+    }
+}
+
+
+$dog = new Dog("Buddy");
+$cat = new Cat("Whiskers");
+
+// Output
+echo $dog->introduce() . "\n";
+echo $dog->makeSound() . "\n";
+echo $dog->move() . "\n";
+
+echo $cat->introduce() . "\n";
+echo $cat->makeSound() . "\n";
+echo $cat->move() . "\n";
